2445b5c1dd8034cacd76be67a258f265da0fe20c
Charlie Smith <charlie@chuckus.nz>
2019-04-07 18:40:27 +0100

    Bump version: 0.2.16 → 0.3.0

--- setup.py
+++ setup.py
@@ -16,13 +16,13 @@ def readme():
 setup(
     name='chromewhip',
 
-    version='0.2.16',
+    version='0.3.0',
 
     description='asyncio driver + HTTP server for Chrome devtools protocol',
     long_description=readme(),
     # The project's main homepage.
     url='https://github.com/chuckus/chromewhip',
-    download_url='https://github.com/chuckus/chromewhip/archive/v0.2.16.tar.gz',
+    download_url='https://github.com/chuckus/chromewhip/archive/v0.3.0.tar.gz',
 
     # Author details
     author='Charlie Smith',
